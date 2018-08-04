a8b2da269a44193c56befce259ccbc84508a206b
Kevin Falconer <40172641@live.napier.ac.uk>
2018-08-04 16:41:34 -0700

    wtforms validation

--- application.py
+++ application.py
@@ -1,29 +1,39 @@
-from flask import Flask, render_template, abort, request
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_wtf import Form
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SecretKey'
+
+class LoginForm(Form):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+#class RegisterForm(Form):
+    
 @app.route("/")
 def route():
     return render_template('main.html')
 
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    error = None
-    if request.method == 'POST':
-        username = request.form['login']
-        password = request.form['password']
-    return render_template('login.html')
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        return "Form Successfully Submitted"
+    return render_template('login.html', form=form)
+
 
 @app.route("/register/", methods=['GET','POST'])
 def register():
-    error = None
+    form = LoginForm()
     if request.method == 'POST':
-        registerUsername = request.form['username']
-        registerPassword = request.form['password']
-    return render_template('register.html')
+        return "hello"
+    return render_template('register.html', form=form)
 
 @app.errorhandler(404)
 def error_page(error):
     return "Couldn't find the page"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
