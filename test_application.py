b5ac6a07a040784cfa25ca8145f1fed58e5c38c4
Kevin Falconer <40172641@live.napier.ac.uk>
2018-09-10 10:05:08 -0700

    Test Application dropped all the tables by mistake, so new values added to tables, also addition of Task Completed CSS

--- test_application.py
+++ test_application.py
@@ -1,36 +1,36 @@
 import os
 import unittest
 import tempfile
-import application
-from application import db
+#import application
+#from application import db
 
 class applicationTestCase(unittest.TestCase):
 
     def setUp(self):
         #self.db_fd, application.app.config['DATABASE'] = tempfile.mkstemp()
-        SQLALCHEMY_DATABASE_URI = 'sqlite:////home/kevin/HonoursProject/db.db'
+        #SQLALCHEMY_DATABASE_URI = 'sqlite:////home/kevin/HonoursProject/db.db'
         TESTING = True
         self.app = application.app.test_client()
         with application.app.app_context():
             db.create_all()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        #db.session.remove()
+        #db.drop_all()
 
-    def login(self, username, password):
-        return self.app.post(
-            '/login/', 
-            data=dict(username=username, password=password), 
-            follow_redirects=True
-            )
+    #def login(self, username, password):
+     #   return self.app.post(
+      #      '/login/', 
+       #     data=dict(username=username, password=password), 
+        #    follow_redirects=True
+         #   )
 
-    def register(self, firstname, lastname, email, username, password, confirm):
-        return self.app.post(
-            '/register/',
-            data=dict(firstname=firstname, lastname=lastname, email=email, username=username, password=password),
-            follow_redirects=True
-            )
+#    def register(self, firstname, lastname, email, username, password, confirm):
+ #       return self.app.post(
+  #          '/register/',
+   #         data=dict(firstname=firstname, lastname=lastname, email=email, username=username, password=password, confirm=confirm),
+     #       follow_redirects=True
+    #        )
 
 
     def test_login_display(self):
@@ -39,14 +39,14 @@ class applicationTestCase(unittest.TestCase):
         self.assertIn(b'Please Login to your account', response.data)
         self.assertIn(b"Username", response.data)
     
-    def test_valid_login(self):
-        self.app.get('/login/', follow_redirects=True)
-        response = self.login('ExampleUser1', 'falconer8')
-        self.assertIn(b'ExampleUser1', response.data)
+#    def test_valid_login(self):
+ #       self.app.get('/login/', follow_redirects=True)
+  #      response = self.login('ExampleUser1', 'falconer8')
+   #     self.assertIn(b'ExampleUser1', response.data)
 
-    def test_invalid_login(self):
-        self.app.get('/login/', follow_redirects=True)
-        response = self.login('ExampleUser123', 'ExamplePassword')
+#    def test_invalid_login(self):
+ #       self.app.get('/login/', follow_redirects=True)
+ #      response = self.login('ExampleUser123', 'ExamplePassword')
         #self.assertIn(b'Login Unsuccessful, Please Try Again', response.data)
     
     def test_register_display(self):
