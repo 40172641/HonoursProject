8d7177aace5c4a275ba15c327772776b312f517a
Kevin Falconer <40172641@live.napier.ac.uk>
2018-09-10 09:44:26 -0700

    Expansion of Testing, Including Valid/Invalid Login, and Registeration, currently sitting in main directory due to errors importing when in tests

--- test_application.py
+++ test_application.py
@@ -18,6 +18,43 @@ class applicationTestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
+    def login(self, username, password):
+        return self.app.post(
+            '/login/', 
+            data=dict(username=username, password=password), 
+            follow_redirects=True
+            )
+
+    def register(self, firstname, lastname, email, username, password, confirm):
+        return self.app.post(
+            '/register/',
+            data=dict(firstname=firstname, lastname=lastname, email=email, username=username, password=password),
+            follow_redirects=True
+            )
+
+
+    def test_login_display(self):
+        response = self.app.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please Login to your account', response.data)
+        self.assertIn(b"Username", response.data)
+    
+    def test_valid_login(self):
+        self.app.get('/login/', follow_redirects=True)
+        response = self.login('ExampleUser1', 'falconer8')
+        self.assertIn(b'ExampleUser1', response.data)
+
+    def test_invalid_login(self):
+        self.app.get('/login/', follow_redirects=True)
+        response = self.login('ExampleUser123', 'ExamplePassword')
+        #self.assertIn(b'Login Unsuccessful, Please Try Again', response.data)
+    
+    def test_register_display(self):
+        response = self.app.get('/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please Register your account', response.data)
+        self.assertIn(b"Username", response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_application.py
+++ tests/test_application.py
@@ -1,26 +1,42 @@
 import os
 import unittest
 import tempfile
-import application
+from HonoursProject import application
+from application import db
 
 class applicationTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, application.app.config['DATABASE'] = tempfile.mkstemp()
-        #self.db_fd, application.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kevin/HonoursProject/database.db'
-        application.app.testing = True
+        #self.db_fd, application.app.config['DATABASE'] = tempfile.mkstemp()
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////home/kevin/HonoursProject/db.db'
+        TESTING = True
         self.app = application.app.test_client()
         with application.app.app_context():
-            application.init_db()
+            db.create_all()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(application.app.config['DATABASE'])
+        db.session.remove()
+        db.drop_all()
 
-    def test_empty_db(self):
-        rv = self.app.get('/')
-        assert b'No entries so far' in rv.data
+    def login(self, username, password):
+        return self.app.post(
+            '/login/', 
+            data=dict(username=username, password=password), 
+            follow_redirects=True
+            )
+
+    def test_login_display(self):
+        response = self.app.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Please Login to your account', response.data)
+        self.assertIn(b"Username", response.data)
     
+    def test_valid_login(self):
+        self.app.get('/login/', follow_redirects=True)
+        response = self.login('ExampleUser1', 'falconer8')
+        self.assertIn(b'ExampleUser1', response.data)
+
+
 if __name__ == '__main__':
     unittest.main()
 
