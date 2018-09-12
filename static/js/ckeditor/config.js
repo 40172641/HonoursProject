b53431097da6f150062e786ec5afdcc07acd8a46
Kevin Falconer <40172641@live.napier.ac.uk>
2018-09-12 10:16:33 -0700

    Attempted to implement a Second Rich Text Editor, as Froala is still changing the CSS on the actual template webpage

---
+++ static/js/ckeditor/config.js
@@ -0,0 +1,17 @@
+/*
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+config.allowedContent = {
+        script: true,
+        $1: {
+            // This will set the default set of elements
+            elements: CKEDITOR.dtd,
+            attributes: true,
+            styles: true,
+            classes: true
+        }
+    };
+};
