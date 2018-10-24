3e79a523f0fa6df5e94351093714537cc5adeefa
Kevin Falconer <40172641@live.napier.ac.uk>
2018-10-24 08:59:27 -0700

    More Unit Testing

--- test.py
+++ test.py
@@ -42,11 +42,20 @@ class TestApplication(unittest.TestCase):
             data=dict(firstname=firstname, lastname=lastname, email=email, username=username, password=password, confirm=confirm),
             follow_redirects=True
             )
+    
+    def logout(self):
+        return self.app.get('/logout/',
+                follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
     def test_valid_login(self):
         self.app.get('/login/', follow_redirects=True)
-        response = self.login('ExampleUser1', 'falconer8')
-        self.assertIn(b'ExampleUser1', response.data)
+        response = self.login('ExampleUser70', 'examplepassword')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'ExampleUser70', response.data)
+        response1 = self.app.get('/dashboard/ExampleUser70/', follow_redirects=True)
+        self.assertEqual(response1.status_code, 200)
+        self.assertIn(b'ExampleUser70', response1.data)
 
 
     def test_invalid_login(self):
@@ -54,6 +63,20 @@ class TestApplication(unittest.TestCase):
         response = self.login('ExampleUser123', 'ExamplePassword')
         self.assertIn(b'Login Unsuccessful, Please Try Again', response.data)
 
+    
+    def test_valid_registeration(self):
+        self.app.get('/register/', follow_redirects=True)
+        response = self.register('Kevin', 'Falconer', 'ExampleEmail1234@gmail.com', 'ExampleEmail1', 'examplePassword', 'examplePassword')
+        self.assertEqual(response.status_code, 200)
+
+    def test_valid_registeration(self):
+        self.app.get('/register/', follow_redirects=True)
+        response = self.register('Kevin', 'Falconer', 'exampleexa132@email.com', 'ExampleEmail12', 'examplePassword', 'examplePassword')
+        self.assertIn(b'Email Already Exists', response.data)
+
+    def test_dashboard_without_login(self):
+        response = self.app.get('/dashboard/ExampleUser70/', follow_redirects=True)
+        self.assertIn(b'Please Login to your account', response.data)
 
 if __name__ == '__main__':
     unittest.main()
