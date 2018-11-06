8c2b57f121ea7eca9f191fc02ea278dd775d9f49
Kevin Falconer <40172641@live.napier.ac.uk>
2018-11-06 05:38:17 -0800

    test

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, flash, session, redirect, abort, url_for,jsonify, json
 import json
 import random
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 import requests
 from werkzeug.security import generate_password_hash, check_password_hash
 import copy
